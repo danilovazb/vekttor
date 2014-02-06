91c92439885845ccddf68cbb731e37eb20d019ff
Ricardo <ricardolima89@gmail.com>
2014-02-06 09:47:58 -0400

    Criacao da tabela paineis_planos

---
+++ The Voice/tabelas/paineis_planos.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: Fev 06, 2014 as 01:44 PM
+-- Versão do Servidor: 5.1.44
+-- Versão do PHP: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `nv_sistema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `paineis_planos`
+--
+
+CREATE TABLE IF NOT EXISTS `paineis_planos` (
+  `id` int(11) NOT NULL,
+  `paineis_id` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `insercoes` int(11) NOT NULL,
+  `dias` int(11) NOT NULL,
+  `valor` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
